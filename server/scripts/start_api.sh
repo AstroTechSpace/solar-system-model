e7aa242368c8b4904d38b3f04dc583581ec31136
Fakhriddin <fahriddin3040@gmail.com>
2024-10-06 17:17:33 +0500

    PUSH it
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- server/scripts/start_api.sh
+++ server/scripts/start_api.sh
@@ -3,4 +3,4 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
-python3 manage.py runserver 0.0.0.0:"${DJANGO_PORT}"
+python3 manage.py runserver ${DJANGO_HOST}:"${DJANGO_PORT}"
